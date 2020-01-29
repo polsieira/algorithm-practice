9385d59dbae226d6ae20e6988dc06730e951480d
posi7790 <posi7790@colorado.edu>
2020-01-29 14:12:59 -0700

    Complete fizzbuzz exercise

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,10 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    i % 3 === 0 ? i % 5 === 0 ? console.log('fizzbuzz') : console.log('fizz') : i % 5 === 0 ? console.log('buzz') : console.log(i);
+  }
+}
 
 module.exports = fizzBuzz;
