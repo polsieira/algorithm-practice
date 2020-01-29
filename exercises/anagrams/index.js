66c68dee9129366595eb4d1833cd421062ed958f
posi7790 <posi7790@colorado.edu>
2020-01-29 16:49:07 -0700

    Complete anagrams exercise

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,36 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  // Solution 1:
+  const aCharMap = createCharacterMap(stringA);
+  const bCharMap = createCharacterMap(stringB);
+
+  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+    return false;
+  }
+
+  for (let character in aCharMap) {
+    if (aCharMap[character] !== bCharMap[character]) {
+      return false;
+    }
+  }
+
+  return true;
+
+  // Solution 2:
+  // return cleanString(stringA) === cleanString(stringB);
+}
+
+const createCharacterMap = (str) => {
+  return str.replace(/[^\w]/g, '').toLowerCase().split('').reduce((map, character) => {
+    map[character] = map[character] ? map[character] + 1 : 1;
+    return map;
+  }, {})
+}
+
+const cleanString = (str) => {
+  return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+}
 
 module.exports = anagrams;
