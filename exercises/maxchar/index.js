5576673bbc791d0b8683e1737643f7662efc60aa
posi7790 <posi7790@colorado.edu>
2020-01-29 14:02:25 -0700

    Complete maxChar exercise

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,29 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  // Solution 1:
+  const count = str.split('').reduce((acc, character) => {
+    if (!acc[character]) {
+      acc[character] = 1;
+    } else {
+      acc[character]++;
+    }
+    return acc
+  }, {})
+
+  let maxCharacter;
+
+  Object.keys(count).forEach((character, index) => {
+    if (index === 0) {
+      maxCharacter = character;
+    } else {
+      if (count[character] > maxCharacter) {
+        maxCharacter = character;
+      }
+    }
+  })
+  return maxCharacter;
+}
 
 module.exports = maxChar;
