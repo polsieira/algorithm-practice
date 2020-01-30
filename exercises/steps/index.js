ff748a8242e53e4c760f15b4a2edc8ad50f502b2
posi7790 <posi7790@colorado.edu>
2020-01-29 17:53:09 -0700

    Complete steps exercise

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,36 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n, row = 0, stair = '') {
+  // Solution 1:
+  // for (let row = 0; row < n; row++) {
+  //   let stair = '';
+  //   for (let col = 0; col < n; col++) {
+  //     if (col <= row) {
+  //       stair = stair + '#'
+  //     } else {
+  //       stair = stair + ' '
+  //     }
+  //   }
+  //   console.log(stair)
+  // }
+
+  // Solution 2:
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1)
+  }
+
+  if (stair.length <= row) {
+    stair += '#';
+  } else {
+    stair += ' ';
+  }
+  steps(n, row, stair);
+}
 
 module.exports = steps;
