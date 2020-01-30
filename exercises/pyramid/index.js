631db943830c0311f6946041c35a7082a5238f2e
posi7790 <posi7790@colorado.edu>
2020-01-29 18:16:39 -0700

    Complete pyramid exercise

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,25 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  // Solution 1:
+  const midpoint = Math.floor((2 * n - 1) / 2);
+
+  for (let row = 0; row < n; row++) {
+    let level = '';
+
+    for (let column = 0; column < 2 * n - 1; column++) {
+      if (midpoint - row <= column && midpoint + row >= column) {
+        level += '#'
+      } else {
+        level += ' '
+      }
+    }
+
+    console.log(level)
+  }
+
+  // Solution 2:
+}
 
 module.exports = pyramid;
